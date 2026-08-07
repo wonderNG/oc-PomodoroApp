038256a66fda54609b1f6cbf1e94bb6fe9b6d01f
wonderNG <82185052+wonderNG@users.noreply.github.com>
2026-08-07 11:58:27 +0100

    Revert "Update main.jsx"
    This reverts commit e5e5f4641b79963088b2b251aa39e45341fb7fbd.

--- oc-Pomodoro/src/main.jsx
+++ oc-Pomodoro/src/main.jsx
@@ -13,26 +13,19 @@ import App from "./App";
 import Info from "./Info";
 import Stats from "./Stats";
 
-const isElectron = window.location.protocol === "file:";
+const Router =
+  window.location.protocol === "file:"
+    ? HashRouter
+    : BrowserRouter;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {isElectron ? (
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/info" element={<Info />} />
-          <Route path="/stats" element={<Stats />} />
-        </Routes>
-      </HashRouter>
-    ) : (
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/info" element={<Info />} />
-          <Route path="/stats" element={<Stats />} />
-        </Routes>
-      </BrowserRouter>
-    )}
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/info" element={<Info />} />
+        <Route path="/stats" element={<Stats />} />
+      </Routes>
+    </Router>
   </StrictMode>
 );
